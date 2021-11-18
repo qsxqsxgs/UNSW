0d78896f9e7da10ed58721d379c856ce9c43a0f3
Qsx <sixiang.qiu@stud.th-luebeck.de>
2021-11-18 23:48:00 +0800

    Randomize algorithm

---
+++ COMP9024_Week_10/mypassword.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main() {
+  int  key;
+  int  flag;
+  int  seed, length;
+  char password[33];
+
+  flag = scanf("%d", &seed);
+  assert(flag == 1);
+  srand(seed);
+
+  flag = scanf("%d", &length);
+  assert(flag == 1);
+
+  for (int i = 0; i < length; i++) {
+    key = rand() % (126 - 33 + 1) + 33;
+    password[i] = key;
+  }
+
+  password[length] = '\0';
+  printf("%s", password);
+}
